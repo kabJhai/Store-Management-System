37accfcbc35ccef28bf78fb8555c8623ec63f841
Kabila Haile Soboka <kabilahaile@gmail.com>
2020-07-28 09:57:47 +0300

    Store issue Voucher

--- includes/routes.php
+++ includes/routes.php
@@ -40,4 +40,58 @@ if(isset($_POST['srv'])){
     }
 }
 }
+
+if(isset($_POST['siv'])){
+    $serial_number = $_POST['serial_number'];
+    $did = $_POST['DID'];
+    $issuing_store = $_POST['issuing_store'];
+    $grv_number = $_POST['grv_number'];
+    $code = $_POST['code'];
+    $description = $_POST['description'];
+    $unit = $_POST['unit'];
+    $qty_req = $_POST['qty_requested'];
+    $qty_issued = $_POST['qty_issued'];
+    $unit_price = $_POST['unit_price'];
+    $total_price = $_POST['total_price'];
+    $remark = $_POST['remark'];
+    $store_keeper = $_POST['store_keeper'];
+    $recepient_name = $_POST['recepient_name'];
+    $authorized_by = $_POST['authorized_by'];
+    echo $serial_number;
+    $length = count($code);
+
+    for ($i=0; $i < $length; $i++) { 
+        try {
+            if($query = $DBcon->query("INSERT INTO
+            siv(serial_number,
+            DID,
+            code,
+            description,
+            unit,
+            qty_requested,
+            qty_issued,
+            unit_price,
+            total_price,
+            remark,
+            store_keeper,
+            recepient_name,
+            authorized_by,
+            issuing_store,
+            grv_number)
+           VALUES
+           (".$serial_number.",'".$did."','".$code[$i]."'
+           ,'".$description[$i]."','".$unit[$i]."','".$qty_req[$i]."',
+           '".$qty_issued[$i]."','".$unit_price[$i]."','".$total_price[$i]."',
+           '".$remark[$i]."','".$store_keeper."','".$recepient_name."',
+           '".$authorized_by."','".$issuing_store."',".$grv_number.")")){
+               echo "Successfully saved";
+       }else{
+           echo "There is an error!";
+       }
+   
+         } catch (Exception $e) {
+            return $e->getMessage();
+         }
+}
+}
 ?>
---
+++ siv.php
@@ -0,0 +1,141 @@
+<?php
+include "includes/head.php";
+
+include "includes/navbar.php";
+include "includes/sidebar.php";
+?>
+  <div class="main-panel">
+          <div class="content-wrapper">
+            
+          <div class="col-12">
+                <div class="card">
+                  <div class="card-body">
+                    <form class="form-sample" method="POST" action="includes/routes">
+                    <h4 class="card-title">Store Issue Voucher (SIV)
+                      <span class="record_number">Number: 205032<input style="display:none" name="serial_number" type="text" value="205032"></span>
+                    </h4>
+
+                      <p class="card-description">
+                        <strong>Date <span id="date"></span></strong><br/>
+                        <div class="row">
+                              <div class="col-md-12">
+                                <div class="form-group row">
+                                  <strong class="col-sm-4 col-form-label">Requesting Unit/Site:</strong>
+                                  <div class="col-sm-8">
+                                  <input class="form-control issuing" name="DID" type="text" value="ICT Department">                                  </div>
+                                </div>
+                              </div>
+                        </div>
+                        <div class="row">
+                              <div class="col-md-12">
+                                <div class="form-group row">
+                                  <strong class="col-sm-4 col-form-label">Issuing Store:</strong>
+                                  <div class="col-sm-8">
+                                  <input name="issuing_store" type="text" class="form-control issuing">                                  </div>
+                                </div>
+                              </div>
+                        </div>
+                        <div class="row">
+                              <div class="col-md-12">
+                                <div class="form-group row">
+                                  <strong class="col-sm-4 col-form-label">GRV No:</strong>
+                                  <div class="col-sm-8">
+                                  <input name="grv_number" type="number" class="form-control">                                  </div>
+                                </div>
+                              </div>
+                        </div>
+                        <strong>Items: <span id="item_count">0</span></strong>
+                      <script>
+                        var d = new Date();
+                        document.getElementById('date').innerHTML = d.getDate()+" - "+(d.getUTCMonth()+1)+" - "+d.getFullYear();
+                      </script>
+                        </p>
+                      <div id="form-items-count">
+                        <div class="row">
+                              <div class="col-md-12">
+                                <div class="form-group row">
+                                  <label class="col-sm-4 col-form-label">Number of Items to Order</label>
+                                  <div class="col-sm-8">
+                                    <input type="number" min="0" value=0 id="items-needed" oninput="add()" class="form-control" />
+                                  </div>
+                                </div>
+                              </div>
+                        </div>
+                      </div>
+                      <div id='form-array-container'>
+                      </div>
+
+                      <div class="row">
+                        <div class="col-md-6">
+                          <div class="form-group row">
+                            <label class="col-sm-5 col-form-label">Store Keepers Name</label>
+                            <div class="col-sm-7">
+                            <input type="text" name="store_keeper"  value="Abebe Kebede Chanyalew" class="form-control"/>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="col-md-6">
+                          <div class="form-group row">
+                            <label class="col-sm-3 col-form-label">Recepients Name</label>
+                            <div class="col-sm-9">
+                            <input type="text" name="recepient_name" id="items-needed" class="form-control"/>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div class="row">
+                        <div class="col-md-6">
+                          <div class="form-group row">
+                            <label class="col-sm-5 col-form-label">Authorized By</label>
+                            <div class="col-sm-7">
+                            <input type="text" name="authorized_by"  value="Abebe Kebede Chanyalew" class="form-control"/>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="form-group row">
+                            <div class="col-sm-2">
+                            </div>
+                            <button type="submit" name="siv" class="btn btn-gradient-primary btn-icon-text col-sm-8">
+                                <i class="mdi mdi-file-check btn-icon-prepend"></i> Submit SIV</button>
+                            <div class="col-sm-2">
+                            </div>
+                            </div>
+                        </div>
+                      </div>
+
+                    </form>
+                  </div>
+                </div>
+              </div>
+          </div>
+          <!-- content-wrapper ends -->
+          <!-- partial:../../partials/_footer.html -->
+          <?php
+          include "includes/footer.php"
+          ?>
+          <!-- partial -->
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="assets/vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="assets/js/off-canvas.js"></script>
+    <script src="assets/js/hoverable-collapse.js"></script>
+    <script src="assets/js/misc.js"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page -->
+    <!-- End custom js for this page -->
+  </body>
+</html>
