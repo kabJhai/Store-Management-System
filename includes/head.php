00249c614484fd4a703cb28ae1be118b755b010d
Kabila Haile Soboka <kabilahaile@gmail.com>
2020-08-05 11:55:30 +0300

    Import required libraries

--- includes/head.php
+++ includes/head.php
@@ -9,8 +9,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>TACON-Store Management System</title>
     <!-- plugins:css -->
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+
     <!-- endinject -->
     <!-- Plugin css for this page -->
     <!-- End plugin css for this page -->
@@ -19,6 +23,9 @@
     <!-- Layout styles -->
     <link rel="stylesheet" href="assets/css/style.css">
     <!-- End layout styles -->
+    <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
+    <link href="assets/css/toaster.min.css" rel="stylesheet">
+
     <link rel="shortcut icon" href="assets/images/favicon.png" />
     <script src="assets/js/dynamic.js"></script>
 
